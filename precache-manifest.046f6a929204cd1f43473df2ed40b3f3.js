e9cfdffda1f627bb058d3e657ce25c8f73ab8719
thecerealcoder <thecerealcoder@gmail.com>
2020-05-10 20:34:18 -0500

    Updates

---
+++ precache-manifest.046f6a929204cd1f43473df2ed40b3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27f972cc96e2e2304ed880c63ace753",
+    "url": "/brewguru/index.html"
+  },
+  {
+    "revision": "aa59a64b581ff22d625e",
+    "url": "/brewguru/static/css/main.a5a9402c.chunk.css"
+  },
+  {
+    "revision": "4e52e6f97e8728aba1a9",
+    "url": "/brewguru/static/js/2.f87562b1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/brewguru/static/js/2.f87562b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa59a64b581ff22d625e",
+    "url": "/brewguru/static/js/main.fc3b2c3f.chunk.js"
+  },
+  {
+    "revision": "401c16e8633df0c015dd",
+    "url": "/brewguru/static/js/runtime-main.f5949808.js"
+  }
+]);
